47e8c9ff7e7eef1eeaffe191ee84b272eda98b43
unknown <redoyhasan4585@gmail.com>
2024-03-27 02:41:42 +0400

    full done futher i know

--- assignment-8-project/README.md
+++ assignment-8-project/README.md
@@ -1,8 +1,15 @@
-# React + Vite
+# WellCome to Book Mania
+ 
+ ### Our Live Link 
+ [@]
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
-Currently, two official plugins are available:
+## Why Us 
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- High Quality Books ✅
+- Online Read Facility ✅
+- No Junk Books ❌
+- Only authentic Books ✅
+- Always Update Latest Books ✅
+
+
--- assignment-8-project/src/components/About/About.jsx
+++ assignment-8-project/src/components/About/About.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import { Link } from "react-router-dom";
 
 const About = () => {
   return (
-    <div>
-      about
-    </div>
+    <section className=" bg-gradient-to-r from-lime-100 via-red-100 to-blue-100">
+	<div className="container mx-auto flex flex-col items-center px-4 py-16 text-center md:py-32 md:px-10 lg:px-32 xl:max-w-3xl">
+		<h1 className="text-4xl font-bold leading-none sm:text-5xl">About <span className="text-violet-800">Us</span>
+		</h1>
+     <h3 className=" my-5 font-bold text-xl">Why Visit Our WebSite</h3>
+     <ul className=" list-disc font-bold lg:ml-10">
+      <li>High Quality Books ✅</li>
+      <li>Online Read Facility ✅</li>
+      <li>No Junk Books ❌</li>
+      <li>Only authentic Books ✅</li>
+      <li>Always Update Latest Books ✅</li>
+     </ul>
+		<div className="flex flex-wrap justify-center gap-5 my-5">
+			<Link to={'/'}>
+      <button className=" font-bold rounded bg-violet-400 text-gray-900 btn btn-outline text-lg" >Check Our Books</button>
+      </Link>
+			<button className="text-lg border rounded  border-gray-700 btn btn-info">Contact Us</button>
+		</div>
+	</div>
+</section>
   );
 };
 
